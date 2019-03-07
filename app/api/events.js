a5a11f525cb506bddf12605fb032b124ac032c1a
allanjuliato <allan.juliato@gmail.com>
2019-03-06 23:35:42 -0300

    Funcao da lista e add de eventos

--- app/api/events.js
+++ app/api/events.js
@@ -1,20 +1,40 @@
-/*const pool = require('../../config/db')
+const pool = require('../../config/db')
 
 module.exports = (app) => {
+
+    const Pool = require('pg').Pool
+    const pool = new Pool({
+        user: 'postgres',
+        host: 'localhost',
+        database: 'postgres',
+        password: 'apolo0404',
+        port: 5432,
+    })
+
     const api = {}
 
-    api.list = pool.query('SELECT * FROM events ORDER BY ID_Autor ASC', (error, res) => {
+    api.list = (request, response) => {
+        pool.query('SELECT * FROM events ORDER BY id ASC', (error, results) => {
             if (error) {
                 throw error
             }
-            console.log(res.rows)
+            response.json(results.rows)
         })
-
-    
-
-    api.add = (req, res) => {
-        res.end('Adicionado')
     }
 
+
+
+    api.add = (request, response) => {
+        const { local, datahora, nome, flyer, preco, observacao, datalibera, codpromocao } = request.body
+
+        pool.query('INSERT INTO events (local, datahora , nome, flyer, preco, observacao, datalibera, codpromocao) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)', [local, datahora, nome, flyer, preco, observacao, datalibera, codpromocao], (error, results) => {
+            if (error) {
+                throw error
+            }
+            response.json(results.insertId)
+        })
+    }
+
+
     return api
-}*/
+}
--- app/routes/events.js
+++ app/routes/events.js
@@ -2,28 +2,13 @@ const pool = require('../../config/db')
 
 module.exports = (app) => {
 
-    const Pool = require('pg').Pool
-    const pool = new Pool({
-        user: 'postgres',
-        host: 'localhost',
-        database: 'postgres',
-        password: 'apolo0404',
-        port: 5432,
-    })
+
 
     const api = app.api.events
 
     app.route('/events')
-        .get((request, response) => {
-            pool.query('SELECT * FROM events ORDER BY id ASC', (error, results) => {
-                if (error) {
-                    throw error
-                }
-                response.json(results.rows)
-            })
-        }
-        )
-    //.post(api.add)
+        .get(api.list)
+        .post(api.add)
 }
 
 
